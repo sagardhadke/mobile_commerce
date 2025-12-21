b9a50866539ccb9f049b4a488af4cb5a8c81aa13
Sagar Dhadke <peaceunique2003@gmail.com>
2025-12-21 18:10:31 +0530

    Home Category Items

---
+++ lib/screens/Home/Widget/categoryItems.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_commerce/models/category.dart';
+
+class CategoryItems extends StatelessWidget {
+  const CategoryItems({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 130,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categoriesList.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {},
+            child: Container(
+              padding: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    height: 65,
+                    width: 65,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: AssetImage(categoriesList[index].image),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Text(
+                    categoriesList[index].title,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
