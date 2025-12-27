4b50161e50a12ad32da015ceaa981b3c3077e7cb
Sagar Dhadke <peaceunique2003@gmail.com>
2025-12-27 14:49:52 +0530

    Added New Project Stracture and Api Url with endpints

---
+++ lib/core/constants/app_urls.dart
@@ -0,0 +1,13 @@
+class AppUrls {
+  static const String base_url =
+      "https://ecomapi.projectnest.co.in/ecommerce-api/user";
+
+  static const String login = "$base_url/login";
+  static const String signUp = "$base_url/registration";
+  static const String profile = "$base_url/profile";
+  static const String products = "$base_url/products";
+  static const String createOrder = "$base_url/create-order";
+  static const String viewCart = "$base_url/view-cart";
+  static const String getOrder = "$base_url/get-order";
+  static const String deleteCart = "$base_url/delete-cart";
+}
